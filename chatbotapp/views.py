911a59deac138b7bb3e3f8cc30b81f1f70b2650f
SARAN S <160819663+saran-99629@users.noreply.github.com>
2024-06-05 11:41:33 +0530

    30:25

--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def chatbot(request):
+    if request.method == 'POST':
+        pass
+    
     return render(request, 'chatbot.html')
